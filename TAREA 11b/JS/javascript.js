f79ba056f38de70398cc1f8db28768ea3d3db12c
licsergioantuna <124640783+licsergioantuna@users.noreply.github.com>
2023-03-05 23:59:20 -0300

    Add files via upload

---
+++ TAREA 11b/JS/javascript.js
@@ -0,0 +1,36 @@
+let menu = ["carne", "pescado", "verdura"];
+let eleccion = "verdura";
+let mensaje;
+let numero = false;
+let vueltas = 3;
+
+menu.push("pastas");
+
+
+//for (let i = 0; i < menu.length; i++) {
+  //  console.log(menu[i]);
+//}
+
+
+
+
+    
+
+switch (eleccion) {
+    case "carne" :
+console.log (mensaje = "como bebida te podemos ofrecer vino tinto para acompañar");
+break;
+
+case "pescado" :
+console.log (mensaje = "como bebida te podemos ofrecer vino Blanco para acompañar");
+break;
+
+case "verdura" :
+console.log (mensaje = "como bebida te podemos ofrecer agua mineral para acompañar");
+break;      
+
+default:
+    console.log ("por favor ud debe elejir carne, pescado o verdura para continuar");
+    }
+
+
